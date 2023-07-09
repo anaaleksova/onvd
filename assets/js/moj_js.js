41632c8aefb5e40f22de8a2345e1b3f2fe81f3fb
unknown <ana.aleksova30@gmail.com>
2023-07-09 16:04:28 +0200

    upload folder

---
+++ assets/js/moj_js.js
@@ -0,0 +1,18 @@
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',()=> {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'fa fa-plus')
+        {
+            icon.className="fa fa-minus"
+        }
+        else
+        {
+            icon.className = "fa fa-plus"
+        }
+    })
+})
+
